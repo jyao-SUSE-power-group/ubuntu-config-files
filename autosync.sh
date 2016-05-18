4e6c268d25953f626c16f70f531a1318c36b1fb3
everhopingandwaiting <jackyao713911@gmail.com>
2016-05-18 16:01:32 +0800

    更新 脚本 完善 目录添加

--- autosync.sh
+++ autosync.sh
@@ -6,6 +6,8 @@
 #########################################################################
 #!/bin/bash
 
-DIR=$(uname --nodename)/
-mkdir -p $DIR
-rsync   -avzr  /etc   $DIR
+BASE_NAME=$(uname --nodename)/
+mkdir -p $BASE_NAME
+DIRS="/etc  $(ls /home/$LOGNAME/.*)"
+echo " Don't use  root privileges ！ Now sync ： "  $DIRS
+rsync   -avzr  --progress $DIRS   $BASE_NAME
