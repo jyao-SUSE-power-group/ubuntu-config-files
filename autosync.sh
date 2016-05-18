a0c6f319325fbe93584ef7cafde0bb7baed14302
everhopingandwaiting <jackyao713911@gmail.com>
2016-05-18 20:37:38 +0800

    脚本 最终 修改 路径版

--- autosync.sh
+++ autosync.sh
@@ -6,8 +6,37 @@
 #########################################################################
 #!/bin/bash
 
-BASE_NAME=$(uname --nodename)/
+BASE_NAME=$(uname --nodename)
 mkdir -p $BASE_NAME
-DIRS="/etc  $(ls /home/$LOGNAME/.*)"
+HOME_DIR=/home/$LOGNAME/
+DIRS="/etc  $(/bin/ls -AF $HOME_DIR | grep '^\.')"
 echo " Don't use  root privileges ！ Now sync ： "  $DIRS
-rsync   -avzr  --progress $DIRS   $BASE_NAME
+for dir in $DIRS; do
+  #statements
+  #  eval $(du -s   /home/john/.oh-my-zsh   | sort -nr | awk '{printf("var1=%s", $1)}')
+  #  eval $(du -s   $HOME_DIR$dir   | sort -nr | awk '{printf("var1=%s", $1)}')
+  eval $(du -s   $HOME_DIR$dir   | sort -nr | awk '{printf("file_or_dir_size=%s", $1)}')
+  echo current file_or_dir_size is : $file_or_dir_size
+#let file_or_dir_size=$file_or_dir_size + 10
+ file_or_dir_size_tmp=`expr $file_or_dir_size + 10`
+  if [ $file_or_dir_size_tmp -le 20000 ]; then
+    #statements
+    echo 小于  20M
+      if [ -d $dir ]; then
+        #statements
+        echo 目录存在
+      else
+          echo 目录不存在
+       dir=$HOME_DIR$dir
+       echo    $dir
+      fi
+       echo  当前备份    $dir 目录或文件
+      rsync   -avzR  --progress $dir   $BASE_NAME
+
+  else
+    echo  大于 20M 不予备份
+  fi
+
+done
+
+#rsync   -avzr  --progress $DIRS   $BASE_NAME
